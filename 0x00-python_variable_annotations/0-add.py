8a509df013af8056ae807dc6b98fa0ee355540ef
ManNjoro <elijohnmwoho@gmail.com>
2023-12-07 13:59:35 +0300

    first commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Simple script with an 'add' function to perform addition of two numbers.
+'''
+
+
+def add(a: float, b: float) -> float:
+    '''
+    Adds two numbers and returns the result.
+
+    Parameters:
+    - a (float): The first number.
+    - b (float): The second number.
+
+    Returns:
+    float: The sum of a and b.
+    '''
+    return a + b
