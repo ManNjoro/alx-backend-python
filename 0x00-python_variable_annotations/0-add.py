dff3c4de14f592b416f5066c1097466b389427a2
ManNjoro <elijohnmwoho@gmail.com>
2023-12-07 15:06:01 +0300

    Added README

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -7,12 +7,5 @@ Simple script with an 'add' function to perform addition of two numbers.
 def add(a: float, b: float) -> float:
     '''
     Adds two numbers and returns the result.
-
-    Parameters:
-    - a (float): The first number.
-    - b (float): The second number.
-
-    Returns:
-    float: The sum of a and b.
     '''
     return a + b
