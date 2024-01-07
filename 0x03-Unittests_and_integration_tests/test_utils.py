c0b3a4d4942fed46901399793dfb3f548c7cfa2b
ManNjoro <elijohnmwoho@gmail.com>
2024-01-07 11:16:07 +0300

    Add parameterized test cases for `access_nested_map` to test for `KeyError` exceptions.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -25,3 +25,12 @@ class TestAccessNestedMap(unittest.TestCase):
         output.
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([({}, ("a",), KeyError),
+                           ({"a": 1}, ("a", "b"), KeyError)])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """
+        Tests for keyError
+        """
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
