787ee45757ba24a1b125f46566a09253a6cba768
ManNjoro <elijohnmwoho@gmail.com>
2024-01-07 11:05:55 +0300

    assertEqual

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+unitest using @parameterized decorator
+"""
+
+from parameterized import parameterized, parameterized_class
+from utils import access_nested_map
+import unittest
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test the functionality of access_nested_map
+    function using @parameterized decorator
+    """
+
+    @parameterized.expand([({"a": {"b": 2}}, ("a", "b"), 2),
+                           ({"a": 1}, ("a",), 1),
+                           ({"a": {"b": 2}}, ("a",), {"b": 2})])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """
+        This function tests the 'access_nested_map' function by providing
+        it with a nested map, a path, and an expected output. It then checks
+        if the output of the 'access_nested_map' function matches the expected
+        output.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
